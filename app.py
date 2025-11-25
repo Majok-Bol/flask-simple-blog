f1ed0e36f61e7b546bf65756af3e4be996a21ccb
Majok-Bol <bolmajok001@gmail.com>
2025-11-25 20:58:53 +0300

    built login and register page ui

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
+
 from flask import Flask,render_template,url_for,redirect,flash
+#import register,login forms
+from forms import RegisterForm,LoginForm
+from dotenv import load_dotenv
+import os
 #initialize app with flask
 app=Flask(__name__)
+#load env files
+load_dotenv('.env')
+#use secret key
+app.config['SECRET_KEY']=os.getenv('SECRET_KEY')
 @app.route('/')
 @app.route('/home',methods=['POST','GET'])
 def home():
     return render_template('home.html')
 
+#handle register route
+@app.route('/register',methods=['POST','GET'])
+def register():
+    form=RegisterForm()
+    if form.validate_on_submit():
+        #redirect to login page
+        return redirect(url_for('login',form=form))
+    return render_template('register.html',form=form)
 
+#handle login
+@app.route('/login',methods=['POST','GET'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for('dashboard'))
+    return render_template('login.html',form=form)
 
+@app.route('/dashboard',methods=['POST','GET'])
+def dashboard():
+    msg='Welcome to your dashboard'
+    return render_template('dashboard.html',msg=msg)
 if __name__=='__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 
 #import form fields 
-from wtforms import StringField,BooleanField,EmailField,PasswordField
+from wtforms import StringField,BooleanField,EmailField,PasswordField,SubmitField
 #import validators for form input
 from wtforms.validators import InputRequired,EqualTo,Email,Length
 from flask_wtf import FlaskForm
@@ -10,8 +10,11 @@ class RegisterForm(FlaskForm):
     email=EmailField('Email address',validators=[InputRequired(),Email(),Length(max=50)])
     password=PasswordField('Password',validators=[InputRequired(),Length(min=8,max=255)])
     confirm_password=PasswordField('Confirm password',validators=[InputRequired(),Length(min=8,max=255),EqualTo('password',message='Passwords must match')])
+    submit=SubmitField('Register')
 
 #login form
 class LoginForm(FlaskForm):
     username=StringField('Username',validators=[InputRequired()])
     password=PasswordField('Password',validators=[InputRequired(),Length(min=8)])
+    remember_me=BooleanField('Remember me')
+    submit=SubmitField('Login')
